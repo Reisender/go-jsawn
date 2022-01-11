56f231b04f47204aedfd420068525ecc96dc9126
Doug Weber <dweber@reisenderlabs.net>
2022-01-11 18:52:45 +0100

    saving progress

--- unmarshal.go
+++ unmarshal.go
@@ -2,24 +2,64 @@ package jsawn
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"reflect"
+	"runtime"
 )
 
 // that jsawn tag value for optional fields
 const TagOptional = "optional"
 
 type ParseWarning struct {
-	json.UnmarshalTypeError
+	Warnings []*json.UnmarshalTypeError
 }
 
 func (w ParseWarning) Error() string {
-	return "parse-warning: " + w.UnmarshalTypeError.Error()
+	// pick the first one for now
+	warnings := ""
+	newline := ""
+	for _, warn := range w.Warnings {
+		warnings = fmt.Sprintf("%s%s%s", warnings, newline, warn.Error())
+		newline = "\n"
+	}
+
+	plurality := "warning"
+	if len(w.Warnings) > 1 {
+		plurality += "s"
+	}
+
+	return fmt.Sprintf("%d parse %s\n%s",
+		len(w.Warnings),
+		plurality,
+		warnings,
+	)
 }
 
 func Unmarshal(data []byte, val interface{}) error {
 	rv := reflect.ValueOf(val)
 	if rv.Kind() != reflect.Ptr || rv.IsNil() {
 		return &json.InvalidUnmarshalError{Type: reflect.TypeOf(val)}
+	}
+
+	v := rv.Elem() // get the elem of the val pointer
+	//vi := reflect.Indirect(rv.Elem())
+	vt := v.Type()
+
+	if v.Kind() == reflect.Ptr {
+		//v = vi
+	}
+
+	// hand off to standard unmarshal if not a struct
+	// or a struct pointer
+	if v.Kind() != reflect.Struct {
+		// not a struct so process like normal
+		return json.Unmarshal(data, val)
+	}
+
+	// use custom Unmarshal if it exists
+	if _, ok := val.(json.Unmarshaler); ok {
+		return json.Unmarshal(data, val)
 	}
 
 	rawMap := map[string]json.RawMessage{}
@@ -29,34 +69,58 @@ func Unmarshal(data []byte, val interface{}) error {
 		return err
 	}
 
-	v := reflect.ValueOf(val).Elem() // get the elem of the val pointer
-
-	var pwarn *ParseWarning
+	// collect any parse warnings
+	// which are errors that happen on optional fields
+	pwarn := &ParseWarning{}
 
 	// look at each field and parse and
 	// don't error out on optional fields
 	for i := 0; i < v.NumField(); i++ {
 		f := v.Field(i)
-		ft := v.Type().Field(i)
+		ft := vt.Field(i)
 
 		customJsonTag := ft.Tag.Get("jsawn")
 		jsonTag := ft.Tag.Get("json")
 
 		newVal := reflect.New(f.Type()) // New() returns ptr to new val of f.Type()
 
-		if raw, ok := rawMap[jsonTag]; ok {
-			err := json.Unmarshal(raw, newVal.Interface())
-			if err != nil {
-				if customJsonTag != TagOptional {
-					return err
-				}
+		if jsonTag == "fifth" {
+			runtime.Breakpoint()
+		}
 
-				// is warning err so capture and move on
-				pwarn = &ParseWarning{json.UnmarshalTypeError{
-					Value: string(raw),
-					Type:  ft.Type,
-				}}
-				continue
+		if raw, ok := rawMap[jsonTag]; ok {
+			err := Unmarshal(raw, newVal.Interface()) // recursive
+			if err != nil {
+				var parseWarn *ParseWarning
+				if errors.As(err, &parseWarn) {
+					// add nested warnings
+					for _, warn := range parseWarn.Warnings {
+						warn.Field = ft.Name + "." + warn.Field
+						warn.Struct = vt.Name()
+						pwarn.Warnings = append(pwarn.Warnings, warn)
+					}
+				} else {
+					// not a nested warning so see if it is a warning or err
+					if customJsonTag != TagOptional {
+						return err
+					}
+
+					// is warning err so capture and move on
+					var parseErr *json.UnmarshalTypeError
+					if errors.As(err, &parseErr) {
+						parseErr.Field = jsonTag
+						parseErr.Struct = vt.Name()
+						pwarn.Warnings = append(pwarn.Warnings, parseErr)
+					} else {
+						pwarn.Warnings = append(pwarn.Warnings, &json.UnmarshalTypeError{
+							Value:  string(raw),
+							Type:   ft.Type,
+							Struct: vt.Name(),
+							Field:  jsonTag,
+						})
+					}
+					continue
+				}
 			}
 
 			if !f.CanSet() {
@@ -66,6 +130,9 @@ func Unmarshal(data []byte, val interface{}) error {
 		}
 	}
 
-	return pwarn
-	//return &ParseWarning{}
+	if len(pwarn.Warnings) > 0 {
+		return pwarn
+	}
+
+	return nil
 }
--- unmarshal_test.go
+++ unmarshal_test.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"testing"
+	"time"
 
 	"go-jsawn"
 )
@@ -31,7 +33,7 @@ func Example_theProblem() {
 	jsonStr := []byte(`{
 		"first": "foo",
 		"third": "bad value",
-		"second": "baz"
+		"second": "missing"
 	}`)
 
 	err := json.Unmarshal(jsonStr, &data)
@@ -62,7 +64,7 @@ func ExampleUnmarshal() {
 	jsonStr := []byte(`{
 		"first": "foo",
 		"third": "bad value",
-		"second": "baz"
+		"second": "not missing anymore"
 	}`)
 
 	err := jsawn.Unmarshal(jsonStr, &data)
@@ -81,6 +83,133 @@ func ExampleUnmarshal() {
 	fmt.Println(string(newStr))
 
 	// Output:
-	// parse-warning: json: cannot unmarshal "bad value" into Go value of type jsawn_test.Custom
-	// {"first":"foo","second":"baz","third":""}
+	// 1 parse warning
+	// json: cannot unmarshal "bad value" into Go value of type jsawn_test.Custom
+	// {"first":"foo","second":"not missing anymore","third":""}
+}
+
+type testCase struct {
+	Raw  []byte
+	Want interface{}
+	Got  interface{}
+}
+
+type subStruct struct {
+	FirstName  string `json:"fname"`
+	MiddleName string `json:"mname" jsawn:"optional"`
+	LastName   string `json:"lname"`
+}
+
+func TestUnmarshal(t *testing.T) {
+
+	t.Run("expect a pointer", func(t *testing.T) {
+		var data int
+		err := jsawn.Unmarshal([]byte("42"), data) // not a pointer
+		_, ok := err.(*json.InvalidUnmarshalError) // should be err
+		if !ok {
+			t.Errorf("\nwant err type:\n%T\ngot:\n%T\n",
+				(*json.InvalidUnmarshalError)(nil),
+				err,
+			)
+		}
+	})
+
+	t.Run("work with string", func(t *testing.T) {
+		var data string
+		want := "foo bar"
+
+		err := jsawn.Unmarshal([]byte(`"`+want+`"`), &data)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if data != want {
+			t.Errorf("\nwant:\n%s\ngot:\n%s\n", want, data)
+		}
+	})
+
+	t.Run("work with int", func(t *testing.T) {
+		var data int
+		want := 42
+
+		err := jsawn.Unmarshal([]byte(`42`), &data)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if data != want {
+			t.Errorf("\nwant:\n%d\ngot:\n%d\n", want, data)
+		}
+	})
+
+	t.Run("work with time", func(t *testing.T) {
+		var data time.Time
+		want := time.Now()
+
+		err := jsawn.Unmarshal([]byte(`"`+want.Format(time.RFC3339)+`"`), &data)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if data.Format(time.RFC3339) != want.Format(time.RFC3339) {
+			t.Errorf("\nwant:\n%v\ngot:\n%v\n", want, data)
+		}
+	})
+
+	t.Run("work with structs", func(t *testing.T) {
+		wantTime, _ := time.Parse(time.RFC3339, "2022-01-10T16:07:37+01:00")
+		data := []struct {
+			First  string     `json:"first"`
+			Second int        `json:"second"`
+			Third  time.Time  `json:"third"`
+			Fourth subStruct  `json:"fourth"`
+			Fifth  *subStruct `json:"fifth"`
+		}{{
+			"foo", 42, wantTime,
+			subStruct{FirstName: "foo", LastName: "bar"},
+			&subStruct{FirstName: "foo", LastName: "bar"},
+		}, {
+			"", 0, time.Now(),
+			subStruct{},
+			nil,
+		}}
+
+		want := data[0]
+		got := data[1]
+
+		jsonStr := []byte(`{
+			"first": "foo",
+			"second": 42,
+			"third": "2022-01-10T16:07:37+01:00",
+			"fourth": {
+				"fname": "foo",
+				"mname": 42,
+				"lname": "bar"
+			},
+			"fifth": {
+				"fname": "foo",
+				"mname": 42,
+				"lname": "bar"
+			}
+		}`)
+
+		err := jsawn.Unmarshal(jsonStr, &got)
+		if err != nil {
+			var parseWarn *jsawn.ParseWarning
+			if errors.As(err, &parseWarn) {
+				fmt.Println(parseWarn)
+			} else {
+				t.Error(err)
+			}
+		}
+
+		if want.First != got.First ||
+			want.Second != got.Second ||
+			want.Third.Format(time.RFC3339) != got.Third.Format(time.RFC3339) ||
+			want.Fourth != got.Fourth ||
+			!reflect.DeepEqual(want.Fifth, got.Fifth) {
+
+			t.Errorf("\nwant:\n%+v\ngot:\n%+v\n", want, got)
+		}
+	})
 }
